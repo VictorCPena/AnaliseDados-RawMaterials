aff65cd231012fccd2eb858b83eec4f3886f0200
Victor Pena <victorcpena1@gmail.com>
2023-01-31 12:08:08 -0300

    commit inicial

---
+++ AnaliseDeDados.py
@@ -0,0 +1,146 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+import random as random
+
+
+df = pd.read_csv("C:/Users/Downloads/agricultural_raw_material.csv")
+df.info()
+df.isnull().sum()
+
+# Trocando %, "," por "-"
+
+df = df.replace('%', '', regex=True)
+df = df.replace(',', '', regex=True)
+df = df.replace('-', '', regex=True)
+df = df.replace('', np.nan)
+df = df.replace('MAY90', np.nan)
+
+# Descartando roms com valores NaN
+df = df.dropna()
+
+# Checando se foi resolvido
+
+df.isnull().sum()
+
+# Convertendo o tipo de Dado para Float
+
+lst = ["Coarse wool Price", "Coarse wool price % Change", "Copra Price", "Copra price % Change", "Cotton price % Change","Fine wool Price", "Fine wool price % Change", "Hard log price % Change", "Hard sawnwood price % Change", "Hide price % change", "Plywood price % Change", "Rubber price % Change", "Softlog price % Change", "Soft sawnwood price % Change", "Wood pulp price % Change"]
+df[lst] = df[lst].astype("float")
+df.dtypes
+print(df)
+df.Month  = pd.to_datetime(df.Month.str.upper(), format='%b%y', yearfirst=False)
+
+# Indexando Mês
+
+df = df.set_index('Month')
+print(df)
+
+# Análise Exploratória
+
+sns.set_style('darkgrid')
+matplotlib.rcParams['font.size'] = 14
+matplotlib.rcParams['figure.figsize'] = (9, 5)
+matplotlib.rcParams['figure.facecolor'] = '#00000000'
+
+# Lista de materias-primas
+raw_data=['Coarse wool Price',  'Copra Price','Cotton Price', 'Fine wool Price',  'Hard log Price', 'Hard sawnwood Price',
+ 'Hide Price', 'Plywood Price', 'Rubber Price', 'Softlog Price', 'Soft sawnwood Price', 'Wood pulp Price']
+
+# Gerando a Matrix-Correlação
+
+corrmat = df[raw_data].corr()
+
+# Setando o tamanho do Plot
+
+fig = plt.figure(figsize = (12, 9))
+
+# Mascarando a parte superior do traingle, uma vez que a matriz é simétrica
+
+mask = np.triu(np.ones_like(corrmat, dtype=bool))
+sns.heatmap(corrmat, vmax = .8,mask=mask, square = True, annot = True)
+plt.show()
+
+plt.figure(figsize=(30,15))
+changelist=['Copra price % Change','Softlog price % Change','Rubber price % Change','Cotton price % Change','Coarse wool price % Change','Fine wool price % Change','Hard log price % Change','Hard sawnwood price % Change','Hide price % change','Plywood price % Change','Soft sawnwood price % Change','Wood pulp price % Change']
+
+# Gerando uma matriz de correlação para todo o conjunto de Dados
+
+corrMatrix = df[changelist].corr()
+sns.heatmap(corrMatrix, annot=True)
+plt.show()
+
+axes=df[["Coarse wool Price", "Coarse wool price % Change"]].plot(figsize=(11, 9), subplots=True, linewidth=1)
+
+df[["Wood pulp Price", "Wood pulp price % Change"]].hist(figsize=(11, 9), linewidth=1)
+
+changelist=['Copra price % Change','Softlog price % Change','Rubber price % Change','Cotton price % Change','Coarse wool price % Change','Fine wool price % Change','Hard log price % Change','Hard sawnwood price % Change','Hide price % change','Plywood price % Change','Soft sawnwood price % Change','Wood pulp price % Change']
+for i in range(len(changelist)):
+    plt.figure(figsize=(12,12))
+    df[changelist[i]].hist(figsize=(11, 9), linewidth=1)
+    plt.xlabel('% Change')
+    plt.ylabel('count')
+    plt.legend(changelist[i:],loc='upper center',bbox_to_anchor=(1.2,1))
+
+plt.figure(figsize=(10, 10))
+materialslist=['Copra Price','Softlog Price','Rubber Price','Cotton Price','Coarse wool Price','Fine wool Price','Hard log Price','Hard sawnwood Price','Hide Price','Plywood Price','Soft sawnwood Price','Wood pulp Price']
+for i in range(len(materialslist)):
+    plt.subplot(4,3,i+1)
+    plt.subplots_adjust( hspace=1 ,wspace=0.5)
+    plt.title(materialslist[i])
+    plt.plot(df[materialslist[i]])
+    plt.xticks(rotation=90)
+plt.suptitle("Raw-Materials price comparision")
+
+plt.figure(figsize=(10, 10))
+plt.plot(df[['Cotton Price','Rubber Price']])
+plt.title("Raw-Materials price comparision")
+plt.xlabel('Years')
+plt.ylabel('Prices')
+plt.legend(['Cotton Price','Rubber Price'],loc='upper center',bbox_to_anchor=(1.2,1))
+
+plt.figure(figsize=(10, 10))
+plt.plot(df[['Copra Price','Softlog Price','Rubber Price','Cotton Price','Coarse wool Price','Fine wool Price','Hard log Price','Hard sawnwood Price','Hide Price','Plywood Price','Soft sawnwood Price','Wood pulp Price']])
+plt.title("Raw-Materials price comparision")
+plt.xlabel('Years')
+plt.ylabel('Prices')
+plt.legend(['Copra Price','Softlog Price','Rubber Price','Cotton Price','Coarse wool Price','Fine wool Price','Hard log Price','Hard sawnwood Price','Hide Price','Plywood Price','Soft sawnwood Price','Wood pulp Price'],loc='upper center',bbox_to_anchor=(1.2,1))
+
+plt.figure(figsize=(12, 12))
+for i in range(len(changelist)):
+    r = random.random()
+    b = random.random()
+    g = random.random()
+    color = (r, g, b)
+    plt.subplot(4, 3, i + 1)
+    plt.subplots_adjust(hspace=1, wspace=0.5)
+    plt.plot(df[changelist[i]], c=color)
+    plt.xticks(rotation=90)
+    plt.title(changelist[i])
+    plt.xlabel('Years')
+    plt.ylabel('% Change')
+
+plt.figure(figsize=(12,12))
+sns.scatterplot(df['Cotton Price'],df.index,hue=df['Fine wool Price']);
+lowlist = ['Cotton Price', 'Hide Price', 'Softlog Price', 'Hard log Price', 'Soft sawnwood Price', 'Rubber Price']
+plt.figure(figsize=(12, 12))
+plt.ylabel('Prices')
+plt.xlabel('Years')
+plt.legend(lowlist,loc='upper center',bbox_to_anchor=(1.2,1))
+for i in range(len(lowlist)):
+    sns.scatterplot(y=df[lowlist[i]], x=df.index);
+    plt.legend(lowlist, loc='upper center', bbox_to_anchor=(1.2, 1))
+
+plt.figure(figsize=(12,12))
+sns.scatterplot(df['Cotton Price'],df.index,hue=df['Fine wool Price']);
+lowlist=['Cotton Price','Hide Price','Softlog Price','Hard log Price','Soft sawnwood Price','Plywood Price']
+highlist=['Coarse wool Price','Copra Price','Fine wool Price','Hard sawnwood Price','Plywood Price','Wood pulp Price']
+lowchangelist=['Cotton price % Change','Hide price % change','Softlog price % Change','Hard log price % Change','Soft sawnwood price % Change','Plywood price % Change']
+plt.figure(figsize=(12,12))
+plt.xlabel('Prices')
+plt.ylabel('Years')
+for i in range(len(highlist)):
+    sns.scatterplot(x=df[highlist[i]],y=df.index);
+    plt.legend(highlist,loc='upper center',bbox_to_anchor=(1.2,1))
